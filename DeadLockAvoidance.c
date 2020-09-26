bcabfd9d3c6ea8d241540e6cb3d3ef6d0f86cd1c
Jayanthi-T <66419308+Jayanthi-T@users.noreply.github.com>
2020-09-26 23:25:01 +0530

    Update DeadLockAvoidance.c

--- DeadLockAvoidance.c
+++ DeadLockAvoidance.c
@@ -5,7 +5,7 @@ int main()
 int Max[10][10], need[10][10], alloc[10][10], avail[10], completed[10], safeSequence[10];
 int p, r, i, j, process, count;
 count = 0;
-printf("Enter the no of processes : ");
+printf("Enter the no of processes : "); 
 scanf("%d", &p);
 for(i = 0; i< p; i++)  
 completed[i] = 0;  
